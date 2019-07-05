a84b91c7bfdec1eefde6901079f17864c4fdf872
wyu <kharma@gmail.com>
2019-07-04 23:15:38 -0700

    update api

--- flask-mysql-docker/app/app.py
+++ flask-mysql-docker/app/app.py
@@ -5,29 +5,26 @@ import json
 
 app = Flask(__name__)
 
-
-def favorite_colors() -> List[Dict]:
+def votes() -> List[Dict]:
     config = {
         'user': 'root',
         'password': 'root',
         'host': 'db',
         'port': '3306',
-        'database': 'knights'
+        'database': 'wevo'
     }
     connection = mysql.connector.connect(**config)
     cursor = connection.cursor()
-    cursor.execute('SELECT * FROM favorite_colors')
-    results = [{name: color} for (name, color) in cursor]
+    cursor.execute('SELECT * FROM votes')
+    results = [{project: vote} for (project, vote, comments) in cursor]
     cursor.close()
     connection.close()
 
     return results
 
-
 @app.route('/')
 def index() -> str:
-    return json.dumps({'favorite_colors': favorite_colors()})
-
+    return json.dumps({'votes': votes()})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
