c05231808b94eb5ce9f0796fd86b58089f684871
wyu <kharma@gmail.com>
2019-07-04 23:15:52 -0700

    update db

--- flask-mysql-docker/db/init.sql
+++ flask-mysql-docker/db/init.sql
@@ -1,13 +1,14 @@
-CREATE DATABASE knights;
-use knights;
+CREATE DATABASE wevo;
+use wevo;
 
-CREATE TABLE favorite_colors (
-  name VARCHAR(20),
-  color VARCHAR(10)
+CREATE TABLE votes (
+  project VARCHAR(20),
+  vote VARCHAR(10),
+  comment VARCHAR(255)
 );
 
-INSERT INTO favorite_colors
-  (name, color)
+INSERT INTO votes
+  (project, vote, comment)
 VALUES
-  ('Lancelot', 'blue'),
-  ('Galahad', 'yellow');
+  ('Aggie Square', 'yes', 'Needs additional community feedback.'),
+  ('Aggie Square', 'no', 'May negatively affect residents.');
