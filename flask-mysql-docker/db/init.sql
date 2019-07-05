161095b36a9ada6b60280d5f194a8468902c484e
kharma <kharma@gmail.com>
2019-07-05 12:18:01 -0700

    update sql

--- flask-mysql-docker/db/init.sql
+++ flask-mysql-docker/db/init.sql
@@ -2,13 +2,23 @@ CREATE DATABASE wevo;
 use wevo;
 
 CREATE TABLE votes (
-  project VARCHAR(20),
-  vote VARCHAR(10),
+  project_name VARCHAR(255),
+  vote BOOLEAN,
+  vote_date DATE,
+  lat DECIMAL(10, 4),
+  lng DECIMAL(10, 4),
   comment VARCHAR(255)
 );
 
 INSERT INTO votes
-  (project, vote, comment)
+  (
+    project_name,
+    vote,
+    vote_date,
+    lat,
+    lng,
+    comment
+  )
 VALUES
-  ('Aggie Square', 'yes', 'Needs additional community feedback.'),
-  ('Aggie Square', 'no', 'May negatively affect residents.');
+  ('Aggie Square', 1, '2019-07-01', 38.574740, -121.493580, 'Needs additional community feedback.'),
+  ('Aggie Square', 0, '2019-07-01', 38.574740, -121.493580, 'May negatively affect residents.');
