0a1126214a11e2abac5f785c9b45f9d511416267
wyu <kharma@gmail.com>
2019-07-06 11:16:33 -0700

    separate mvp and flask dir

--- flask-mysql-docker/check_docker_ip.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash/
-docker-machine ip default
--- flask-mysql-docker/run_docker.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-docker-compose down -v
-docker-compose up -d --no-deps --build
-docker-compose up
-
-# Restart machine if it cannot connect during package installation:
-# docker-machine restart
