4191eaf42314be6dd27d53c1dac582d5b517bdf5
kharma <kharma@gmail.com>
2019-07-05 22:28:20 -0700

    add flask-bokeh repo

---
+++ flask-bokeh/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+from flask import render_template
+from bokeh.plotting import figure
+from bokeh.embed import components
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return 'Hello World!'
+
+@app.route('/dashboard/')
+def show_dashboard():
+    plots = []
+    plots.append(make_plot())
+
+    return render_template('dashboard.html', plots=plots)
+
+def make_plot():
+    plot = figure(plot_height=300, sizing_mode='scale_width')
+
+    x = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+    y = [2**v for v in x]
+
+    plot.line(x, y, line_width=4)
+
+    script, div = components(plot)
+    return script, div
