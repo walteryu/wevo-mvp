49a0a5d7eb30ba061f7c21b2f72c797a58c7dc4c
wyu <kharma@gmail.com>
2019-07-07 19:04:59 -0700

    clean up documentation

--- scripts/venv_notes.sh
+++ scripts/venv_notes.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-# initiate venv instance:
+# initiate virtualenv so that packages stay within it:
 virtualenv venv --python=`which python3`
 source venv/bin/activate
 
-# list list dep in requirements.txt:
+# install all packages within requirements.txt:
 pip install -r requirements.txt
 
-# add application path:
+# add application path to start it; set to dev mode:
 export FLASK_APP=app.py
 export FLASK_DEBUG=1
 
-# run application; it should open on port 5000:
+# run application; it opens on port 5000:
 flask run
