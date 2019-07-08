212819b6635043f339e5e5e6e15468eb4086147d
wyu <kharma@gmail.com>
2019-07-07 20:00:37 -0700

    separate dev/prod db settings

--- config.py
+++ config.py
@@ -6,11 +6,10 @@ class Config(object):
     TESTING = False
     CSRF_ENABLED = True
     SECRET_KEY = os.environ['SECRET_KEY']
-    # SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
 
 class ProductionConfig(Config):
     DEBUG = False
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
 
 class StagingConfig(Config):
     DEVELOPMENT = True
@@ -19,6 +18,7 @@ class StagingConfig(Config):
 class DevelopmentConfig(Config):
     DEVELOPMENT = True
     DEBUG = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///app.db'
 
 class TestingConfig(Config):
     TESTING = True
