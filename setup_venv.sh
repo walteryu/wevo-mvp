7b88eea782fd78651d8cd2c17bac7626af3f47f3
wyu <kharma@gmail.com>
2019-07-13 20:27:49 -0700

    organize scripts

---
+++ setup_venv.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# initiate virtualenv so that packages stay within it:
+virtualenv venv --python=`which python3`
+source venv/bin/activate
+
+# install all packages within requirements.txt:
+pip3 install -r requirements.txt
+
+# add application path to start it; set to dev mode:
+export FLASK_APP=app.py
+export FLASK_DEBUG=1
+
+# run application; it opens on port 5000:
+# flask run
