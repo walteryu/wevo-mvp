9a398e5364520fbb25756bffd203ff8a7f04f861
wyu <kharma@gmail.com>
2019-07-20 10:39:22 -0700

    config controller/views

--- manage.py
+++ manage.py
@@ -2,11 +2,13 @@ import os
 from flask_script import Manager
 from flask_migrate import Migrate, MigrateCommand
 
+# original db setup; use sessions for nested resource
 # from app import app, db
 from app import app, s
 
 app.config.from_object(os.environ['APP_SETTINGS'])
 
+# original db setup; use sessions for nested resource
 # migrate = Migrate(app, db)
 migrate = Migrate(app, s)
 manager = Manager(app)
